ce43545874d8c11a4a858dad8e0e2ec7a1657108
Brian Sayler <sayler.b@gmail.com>
2016-12-22 02:14:24 -0500

    add method for building a weighted graph and tests

--- src/main/java/GraphBuilder.java
+++ src/main/java/GraphBuilder.java
@@ -11,14 +11,12 @@ public class GraphBuilder {
     }
 
     public Map<Character, List<Character>> buildUnweighted() {
-
         String[] pairs = input.split(", ");
-        System.out.println(pairs);
 
-        Map<Character, List<Character>> result = new HashMap<Character, List<Character>>();
+        Map<Character, List<Character>> result
+            = new HashMap<Character, List<Character>>();
 
         for (String pair : pairs) {
-            System.out.println(pair);
             char[] charPair = pair.toCharArray();
 
             char origin = charPair[0];
@@ -34,6 +32,34 @@ public class GraphBuilder {
 
             } else {
                 result.get(origin).add(destination);
+            }
+        }
+        return result;
+    }
+
+    public Map<Character, HashMap<Character, Integer>> buildWeighted() {
+        String[] pairs = input.split(", ");
+
+        Map<Character, HashMap<Character, Integer>> result
+            = new HashMap<Character, HashMap<Character, Integer>>();
+
+        for (String pair : pairs) {
+            char[] charPair = pair.toCharArray();
+
+            char origin = charPair[0];
+            char destination = charPair[1];
+            int weight = Character.getNumericValue(charPair[2]);
+
+            HashMap<Character, Integer> adjacentNodes = result.get(origin);
+
+            if (adjacentNodes == null) {
+                adjacentNodes = new HashMap<Character, Integer>();
+                adjacentNodes.put(destination, weight);
+
+                result.put(origin, adjacentNodes);
+
+            } else {
+                result.get(origin).put(destination, weight);
             }
         }
         return result;
