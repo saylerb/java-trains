0697acfcddecc0ae4d79532eac04098ef282a9d3
Brian Sayler <sayler.b@gmail.com>
2016-12-22 01:20:11 -0500

    break long lines and add another test

--- src/test/java/GraphBuilderTest.java
+++ src/test/java/GraphBuilderTest.java
@@ -17,26 +17,47 @@ public class GraphBuilderTest {
 
     @Test
     public void buildUnweightedGraphWithSingleNode() {
-        Map<Character, List<Character>> expected = new HashMap<Character, List<Character>>() {
-            {
-                put('A', Arrays.asList('B'));
-            }
-        };
+        Map<Character, List<Character>> expected
+            = new HashMap<Character, List<Character>>() {
+                {
+                    put('A', Arrays.asList('B'));
+                }
+            };
         GraphBuilder builder = new GraphBuilder("AB");
 
         assertEquals(expected, builder.buildUnweighted());
     }
 
     @Test
-    public void buildUnweightedGraphWithMultipleNodes() {
-        Map<Character, List<Character>> expected = new HashMap<Character, List<Character>>() {
-            {
-                put('A', Arrays.asList('B'));
-                put('B', Arrays.asList('C'));
-                put('C', Arrays.asList('D'));
-            }
-        };
+    public void buildUnweightedGraphWithThreeEdges() {
+        Map<Character, List<Character>> expected
+            = new HashMap<Character, List<Character>>() {
+                {
+                    put('A', Arrays.asList('B'));
+                    put('B', Arrays.asList('C'));
+                    put('C', Arrays.asList('D'));
+                }
+            };
+
         GraphBuilder builder = new GraphBuilder("AB, BC, CD");
+
+        assertEquals(expected, builder.buildUnweighted());
+    }
+
+    @Test
+    public void buildUnweightedGraphWithMultipleRoutes() {
+        Map<Character, List<Character>> expected
+            = new HashMap<Character, List<Character>>() {
+                {
+                    put('A', Arrays.asList('B', 'D', 'E'));
+                    put('B', Arrays.asList('C'));
+                    put('C', Arrays.asList('D', 'E'));
+                    put('D', Arrays.asList('C', 'E'));
+                    put('E', Arrays.asList('B'));
+                }
+            };
+        GraphBuilder builder
+            = new GraphBuilder("AB, BC, CD, DC, DE, AD, CE, EB, AE");
 
         assertEquals(expected, builder.buildUnweighted());
     }
