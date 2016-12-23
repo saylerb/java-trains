a0db053fddb53c10963fc0d28d18da1f966f55b3
Brian Sayler <sayler.b@gmail.com>
2016-12-23 18:11:05 -0500

    extract sample graph to helper static class

---
+++ src/test/java/ExampleGraph.java
@@ -0,0 +1,42 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class ExampleGraph {
+    public static Map<Character, HashMap<Character, Integer>> createGraph() {
+        Map<Character, HashMap<Character, Integer>> thing
+            = new HashMap<Character, HashMap<Character, Integer>>() {
+                {
+                    put('A', new HashMap<Character, Integer>() {
+                        {
+                            put('B', 5);
+                            put('D', 5);
+                            put('E', 7);
+                        }
+                    });
+                    put('B', new HashMap<Character, Integer>() {
+                        {
+                            put('C', 4);
+                        }
+                    });
+                    put('C', new HashMap<Character, Integer>() {
+                        {
+                            put('D', 8);
+                            put('E', 2);
+                        }
+                    });
+                    put('D', new HashMap<Character, Integer>() {
+                        {
+                            put('C', 8);
+                            put('E', 6);
+                        }
+                    });
+                    put('E', new HashMap<Character, Integer>() {
+                        {
+                            put('B', 3);
+                        }
+                    });
+                }
+            };
+        return thing;
+    }
+}
