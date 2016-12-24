b79a6110a3a71ab5222f3a3ee7c5582c59d5adab
Brian Sayler <sayler.b@gmail.com>
2016-12-23 23:57:22 -0500

    :boom: finding paths up to max distance working

--- src/main/java/PathFinder.java
+++ src/main/java/PathFinder.java
@@ -60,6 +60,26 @@ public class PathFinder {
         return depthFirstSearchExactStops(visitedNodes, foundPaths, start);
     }
 
+    public ArrayList<List<Character>> findPathsUpToMaxDistance(
+            Character start,
+            Character end,
+            Integer max) {
+
+        ArrayList<List<Character>> foundPaths = new ArrayList<>();
+
+        ArrayList<Character> visitedNodes = new ArrayList<Character>() {
+            {
+                add(start);
+            }
+        };
+
+        this.start = start;
+        this.end = end;
+        this.max = max;
+
+        return depthFirstSearchUpToDistance(visitedNodes, foundPaths, start);
+    }
+
     private ArrayList<List<Character>> depthFirstSearchExactStops(
             List<Character> visitedNodes,
             ArrayList<List<Character>> foundPaths,
@@ -112,6 +132,37 @@ public class PathFinder {
                         foundPaths,
                         adjacentNode);
             }
+        }
+        return foundPaths;
+    }
+
+    private ArrayList<List<Character>> depthFirstSearchUpToDistance(
+            List<Character> visitedNodes,
+            ArrayList<List<Character>> foundPaths,
+            Character currentNode) {
+
+        String currentDistance
+            = new DistanceCalculator(graph).calculateDistance(visitedNodes);
+
+        if (Integer.parseInt(currentDistance) >= max) {
+            return foundPaths;
+        } else if (currentNode == end && visitedNodes.size() > 1) {
+            foundPaths.add(visitedNodes);
+        }
+
+        List<Character> adjacentNodes = findAdjacentNodes(currentNode);
+
+        for (Character adjacentNode : adjacentNodes) {
+            List<Character> newVisitedNodes = new ArrayList<Character>() {
+                {
+                    addAll(visitedNodes);
+                    add(adjacentNode);
+                }
+            };
+            depthFirstSearchUpToDistance(
+                    newVisitedNodes,
+                    foundPaths,
+                    adjacentNode);
         }
         return foundPaths;
     }
--- src/test/java/PathFinderTest.java
+++ src/test/java/PathFinderTest.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class PathFinderTest {
     @Test
@@ -75,5 +76,35 @@ public class PathFinderTest {
 
         assertEquals(expected, result);
         assertEquals((int)3, result.size());
+    }
+
+    @Test
+    public void testFindingPathsWithinACertainDistance() {
+        Map<Character, HashMap<Character, Integer>> exampleGraph
+            = ExampleGraph.createGraph();
+
+        PathFinder finder = new PathFinder(exampleGraph);
+
+        ArrayList<List<Character>> expectedPaths
+            = new ArrayList<List<Character>>() {
+                {
+                    add(Arrays.asList('C', 'D', 'C'));
+                    add(Arrays.asList('C', 'E', 'B', 'C'));
+                    add(Arrays.asList('C', 'E', 'B', 'C', 'D', 'C'));
+                    add(Arrays.asList('C', 'D', 'C', 'E', 'B', 'C'));
+                    add(Arrays.asList('C', 'D', 'E', 'B', 'C'));
+                    add(Arrays.asList('C', 'E', 'B', 'C', 'E', 'B', 'C'));
+                    add(Arrays.asList('C', 'E', 'B', 'C', 'E', 'B', 'C',
+                                'E', 'B', 'C'));
+                }
+            };
+
+        ArrayList<List<Character>> result
+            = finder.findPathsUpToMaxDistance('C', 'C', 30);
+
+        for (List<Character> path : expectedPaths) {
+            assertTrue(result.contains(path));
+        }
+        assertEquals((int)7, result.size());
     }
 }
