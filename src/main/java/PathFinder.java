e4b74ca128366451ad58e9b5c5020faf2643d215
Brian Sayler <sayler.b@gmail.com>
2016-12-23 22:07:20 -0500

    :hammer: remove some args from recursive calls

--- src/main/java/PathFinder.java
+++ src/main/java/PathFinder.java
@@ -5,6 +5,9 @@ import java.util.HashMap;
 
 public class PathFinder {
     private Map<Character, HashMap<Character, Integer>> graph;
+    private Character start;
+    private Character end;
+    private Integer max;
 
     public PathFinder(Map<Character, HashMap<Character, Integer>> graph) {
         this.graph = graph;
@@ -19,38 +22,38 @@ public class PathFinder {
     }
 
     public ArrayList<List<Character>> findPathWithMax(char start, char end, int max) {
-
         ArrayList<List<Character>> foundPaths = new ArrayList<List<Character>>();
         ArrayList<Character> visitedNodes = new ArrayList<Character>(){{ add(start); }};
 
-        return depthFirstMax(visitedNodes, foundPaths, start, end, max);
+        this.start = start;
+        this.end = end;
+        this.max = max;
+
+        return depthFirstSearchMax(visitedNodes, foundPaths, start);
     }
 
-    private ArrayList<List<Character>> depthFirstMax(
+    private ArrayList<List<Character>> depthFirstSearchMax(
             List<Character> visitedNodes,
-            ArrayList<List<Character>> validRoutesFound,
-            Character currentNode,
-            Character end,
-            Integer max) {
+            ArrayList<List<Character>> foundPaths,
+            Character currentNode) {
 
         if (visitedNodes.size() > max + 1) {
-            return validRoutesFound;
+            return foundPaths;
         } else if (currentNode == end && visitedNodes.size() > 1) {
-            validRoutesFound.add(visitedNodes);
+            foundPaths.add(visitedNodes);
         } else {
             List<Character> adjacentNodes = findAdjacentNodes(currentNode);
 
             for (char adjacentNode : adjacentNodes) {
-                List<Character> temp = new ArrayList<Character>() {
+                List<Character> newVisitedNodes = new ArrayList<Character>() {
                     {
                         addAll(visitedNodes);
                         add(adjacentNode);
                     }
                 };
-
-                depthFirstMax(temp, validRoutesFound, adjacentNode, end, max);
+                depthFirstSearchMax(newVisitedNodes, foundPaths, adjacentNode);
             }
         }
-        return validRoutesFound;
+        return foundPaths;
     }
 }
