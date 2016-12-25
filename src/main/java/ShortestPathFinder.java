2fba11a8edc72d8d8316bb53580132a86f407e4e
Brian Sayler <sayler.b@gmail.com>
2016-12-25 17:16:23 -0500

    :santa: implement lowest code node and find predecessors

--- src/main/java/ShortestPathFinder.java
+++ src/main/java/ShortestPathFinder.java
@@ -6,7 +6,8 @@ import java.util.HashMap;
 public class ShortestPathFinder {
     private Map<Character, HashMap<Character, Integer>> graph;
 
-    public ShortestPathFinder(Map<Character, HashMap<Character, Integer>> graph) {
+    public ShortestPathFinder(
+            Map<Character, HashMap<Character, Integer>> graph) {
         this.graph = graph;
     }
 
@@ -18,7 +19,7 @@ public class ShortestPathFinder {
         return new ArrayList<Character>(graph.get(node).keySet());
     }
 
-    public HashMap<Character, Integer> generateInitialCosts(char start, char end) {
+    public HashMap<Character, Integer> generateCosts(char start, char end) {
         List<Character> nodes = new ArrayList<Character>(graph.keySet());
 
         HashMap<Character, Integer> costs = new HashMap<Character, Integer>();
@@ -32,4 +33,43 @@ public class ShortestPathFinder {
         }
         return costs;
     }
+
+    public HashMap<Character, Character> generatePredecessors(
+            char start,
+            char end) {
+        List<Character> nodes = new ArrayList<Character>(graph.keySet());
+
+        HashMap<Character, Character> parents = new HashMap<>();
+
+        for (char node : nodes) {
+            if (getAdjacentNodes(start).contains(node)) {
+                parents.put(node, start);
+            } else if (node != start) {
+                parents.put(node, null);
+            }
+        }
+        return parents;
+    }
+
+    public Character getLowestCostNode(char start, char end) {
+        Integer lowestCost = Integer.MAX_VALUE;
+        Character lowestCostNode = null;
+
+        HashMap<Character, Integer> costs = generateCosts(start, end);
+
+        List<Character> processed = new ArrayList<Character>();
+
+        for (char node : costs.keySet()) {
+
+            Integer cost = costs.get(node);
+
+            if (cost < lowestCost && !processed.contains(node)) {
+                lowestCost = cost;
+                lowestCostNode = node;
+            }
+        }
+        return lowestCostNode;
+    }
+
+
 }
